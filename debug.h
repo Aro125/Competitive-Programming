975e353f4ed891c501fbeea7f26569d9d9987978
Aro125 <107888846+Aro125@users.noreply.github.com>
2022-06-21 02:24:09 +0600

    Add files via upload

---
+++ debug.h
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void debug(long long x) {cerr << x << " ";}
+void debug(double x) {cerr << x << " ";}
+void debug(int x) {cerr << x << " ";}
+void debug(string &s) {cerr << s << " ";}
+ 
+template <class T> void debug(vector <T> v) {cerr << "[ "; for (T i : v) {debug(i); cerr << " ";} cerr << "]";}
+template <class T> void debug(set <T> v) {cerr << "[ "; for (T i : v) {debug(i); cerr << " ";} cerr << "]";}
+template <class T> void debug(T arr[] ,long long n) {cerr << "[ "; for (long long i = 0; i < n; i++) {debug(arr[i]); cerr << " ";} cerr << "]";}
+void STDIO_SYSTEM(){
+	#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  freopen("Aronno.txt", "w", stderr);
+#endif
+}
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+#ifndef ONLINE_JUDGE
+    #include "debug.h"
+#else
+    #define debug(x)
+#endif
+using namespace std;
+using ll     =   long long; 
+const ll inf =   1e18;
+const ll mod =   1e9 + 7;
+const ll N   =   1e5 + 5;
+#define pb push_back
+#define f first
+#define sc second
+
+ll dx[] = {-1, 1,  0, 0, -1, -1,  1, 1};
+ll dy[] = {0,  0, -1, 1, -1,  1, -1, 1};
+
+
+void testCase() {
+   
+}
+ 
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); 
+#ifndef ONLINE_JUDGE
+ STDIO_SYSTEM();
+#endif
+  ll T = 1; cin >> T;
+  while(T--) testCase();
+  return 0;
+}
