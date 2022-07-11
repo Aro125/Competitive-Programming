4c25af38dcd0ebd2ff13c4566a4080d9a44075fd
Aro125 <107888846+Aro125@users.noreply.github.com>
2022-07-11 16:30:07 +0600

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,27 @@
     #define debug(x)
 #endif
 using namespace std;
-using ll       =   long long; 
-const ll inf   =   1e18;
-const ll mod   =   1e9 + 7;
-const ll N     =   1e5 + 5;
+using ll     =   long long; 
+const ll inf =   1e18;
+const ll mod =   1e9 + 7;
+const ll N   =   1e5 + 5;
 #define pb push_back
-#define f  first
+#define f first
 #define sc second
 
 ll dx[] = {-1, 1,  0, 0, -1, -1,  1, 1};
 ll dy[] = {0,  0, -1, 1, -1,  1, -1, 1};
 
+void bruteForce(ll n) {
+   for(ll i = 1; i <= n; i++) {
+    for(ll j = 1; j <= n; j++) {
+       
+    }
+   }
+}
 
 void testCase() {
-   
+  
 }
  
 signed main() {
