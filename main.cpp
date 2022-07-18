bbef4ba55388974c92f6b110aaa5d085ed6dca74
Aro125 <107888846+Aro125@users.noreply.github.com>
2022-07-18 13:59:50 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #ifndef ONLINE_JUDGE
     #include "debug.h"
 #else
     #define debug(x)
 #endif
 using namespace std;
+using namespace __gnu_pbds;
 using ll     =   long long; 
 const ll inf =   1e18;
 const ll mod =   1e9 + 7;
@@ -15,6 +18,8 @@ const ll N   =   1e5 + 5;
 #define yea cout << "Yes\n"
 #define nope cout << "No\n"
 #define nl cout << endl
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, 
+tree_order_statistics_node_update > pbds; /*find_by_order, order_of_key*/
 
 ll dx[] = {-1, 1,  0, 0, -1, -1,  1, 1};
 ll dy[] = {0,  0, -1, 1, -1,  1, -1, 1};
