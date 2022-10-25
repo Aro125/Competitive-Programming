1b5254049c4be6bbb2026cc039e26dd362dae0f5
Aro125 <107888846+Aro125@users.noreply.github.com>
2022-10-26 01:11:04 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 using namespace __gnu_pbds;
 
 typedef long long ll;
+typedef long double lld;
 typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, 
 tree_order_statistics_node_update > pbds; /*find_by_order, order_of_key*/
 
@@ -25,23 +26,36 @@ tree_order_statistics_node_update > pbds; /*find_by_order, order_of_key*/
 #define set_bits(n) __builtin_popcountll(n)
 #define zeroF_64(n) __builtin_clzll(n)
 #define zeroL_64(n) __builtin_ctzll(n)
+#define flag cout << "****\n"
 
 ll dx[] = {-1, 1,  0, 0, -1, -1,  1, 1};
 ll dy[] = {0,  0, -1, 1, -1,  1, -1, 1};
 
-ll fact(ll n);
-ll nCr(ll n, ll r) {
-  return fact(n) / (fact(r) * fact(n - r));
+ll nCr(ll n, ll r)
+{
+   long long p = 1, k = 1;
+   if (n - r < r)
+        r = n - r;
+   if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+    long long m = __gcd(p, k);
+      p /= m , k /= m , n-- , r--;
+    }
+ }
+ else p = 1;
+    return p;
 }
-ll nPr(ll n, ll r) {
-  return fact(n) / fact(n - r);
-}
+
 ll fact(ll n) {
-  if (n <= 1) return 1; 
-  return (n * fact(n - 1)) % ll(1e9 + 7);
+  if(n > 1) return n * fact(n - 1);
+  else 
+    return 1;
 }
-ll randRange (ll low, ll high) {
-  return rand() % (high - low) + low;
+
+ll nPr(ll n , ll r) {
+  return fact(n) / fact(n - r);
 }
 
 const ll inf =   1e18;
@@ -50,7 +64,11 @@ const ll MOD =   998244353;
 const ll N   =   1e5 + 5;
 const ll PI  =   3.141592653589793238462;
 
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
 mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
 
 void testCase() {
   
